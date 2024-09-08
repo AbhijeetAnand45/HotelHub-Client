9b4202452a0a5e94520afd56643500e342f9281d
Abhijeet Anand <aabhijeet011@gmail.com>
2024-09-08 22:11:45 +0530

    Added Login, Register, Pagination, RoomDetails, AllRooms, BookingPage components

--- src/App.js
+++ src/App.js
@@ -5,6 +5,11 @@ import {BrowserRouter, Routes, Route, Navigate} from 'react-router-dom';
 import Navbar from './components/common/Navbar';
 import HomePage from './components/home/HomePage';
 import FooterComponent from './components/common/Footer';
+import AllRoomsPage from './components/booking_rooms/AllRoomsPage';
+import FindBookingPage from './components/booking_rooms/FindBookingPage';
+import RoomDetailsPage from './components/booking_rooms/RoomDetailsPage';
+import LoginPage from './components/auth/LoginPage';
+import RegisterPage from './components/auth/RegisterPage';
 
 function App() {
   return (
@@ -14,6 +19,12 @@ function App() {
         <div className='content'>
           <Routes>
             <Route exact path='/home' element={<HomePage/>}/>
+            <Route exact path='/rooms' element={<AllRoomsPage/>}/>
+            <Route exact path='/find-booking' element={<FindBookingPage/>}/>
+            <Route exact path='/room-details-book/:roomId' element={<RoomDetailsPage/>}/>
+            <Route exact path='/login' element={<LoginPage/>}/>
+            <Route exact path='/register' element={<RegisterPage/>}/>
+
           </Routes>
         </div>
         <FooterComponent/>
